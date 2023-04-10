8729a49a8582a3a7bf701b806579a4c92d30f80a
Jannis Milz <jannis@milz.ch>
2023-04-10 20:38:26 +0200

    Add pwd echo's at end of script

--- setupEnv.sh
+++ setupEnv.sh
@@ -8,3 +8,7 @@ apt-get install pwgen
 export GHOST_DB_ROOT_PASSWORD=$(pwgen -c 25 -n 1)
 export NPM_DB_PASSWORD=$(pwgen -c 25 -n 1)
 export NPM_DB_ROOT_PASSWORD=$(pwgen -c 25 -n 1)
+
+echo "GHOST_DB_ROOT_PASSWORD: $GHOST_DB_ROOT_PASSWORD"
+echo "NPM_DB_PASSWORD: $NPM_DB_PASSWORD"
+echo "NPM_DB_ROOT_PASSWORD: $NPM_DB_ROOT_PASSWORD"
