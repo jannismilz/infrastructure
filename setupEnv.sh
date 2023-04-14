b132563f00fd38a136992b6979f6e0ee271716bc
Jannis Milz <jannis@milz.ch>
2023-04-14 16:50:44 +0200

    Fix script typo

--- setupEnv.sh
+++ setupEnv.sh
@@ -9,7 +9,7 @@ export GHOST_DB_ROOT_PASSWORD=$(pwgen -c 25 -n 1)
 export NPM_DB_PASSWORD=$(pwgen -c 25 -n 1)
 export NPM_DB_ROOT_PASSWORD=$(pwgen -c 25 -n 1)
 export PLAUSIBLE_DB_PASSWORD=$(pwgen -c 25 -n 1)
-export PLAUSIBLE_SECRET_KEY=${openssl rand -base64 64 | tr -d '\n'}
+export PLAUSIBLE_SECRET_KEY=$(openssl rand -base64 64 | tr -d '\n')
 
 echo "GHOST_DB_ROOT_PASSWORD: $GHOST_DB_ROOT_PASSWORD"
 echo "NPM_DB_PASSWORD: $NPM_DB_PASSWORD"
