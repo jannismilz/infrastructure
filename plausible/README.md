46a5173968a991ea4e1460120f990259cf92e18a
Jannis Milz <jannis@milz.ch>
2023-04-14 16:47:28 +0200

    Add Plausible analytics & extend setup script

---
+++ plausible/README.md
@@ -0,0 +1,8 @@
+Official website: https://plausible.io/
+
+-   **Portainer:** `v2.17.1`
+-   **Last Updated:** `14.04.2023`
+
+# Installation
+
+**Guide with Docker Compose:** https://github.com/plausible/hosting/tree/master
--- setupEnv.sh
+++ setupEnv.sh
@@ -3,12 +3,16 @@
 set -e
 
 apt-get update
-apt-get install pwgen
+apt-get install pwgen openssl
 
 export GHOST_DB_ROOT_PASSWORD=$(pwgen -c 25 -n 1)
 export NPM_DB_PASSWORD=$(pwgen -c 25 -n 1)
 export NPM_DB_ROOT_PASSWORD=$(pwgen -c 25 -n 1)
+export PLAUSIBLE_DB_PASSWORD=$(pwgen -c 25 -n 1)
+export PLAUSIBLE_SECRET_KEY=${openssl rand -base64 64 | tr -d '\n' ; echo}
 
 echo "GHOST_DB_ROOT_PASSWORD: $GHOST_DB_ROOT_PASSWORD"
 echo "NPM_DB_PASSWORD: $NPM_DB_PASSWORD"
 echo "NPM_DB_ROOT_PASSWORD: $NPM_DB_ROOT_PASSWORD"
+echo "PLAUSIBLE_DB_PASSWORD: $PLAUSIBLE_DB_PASSWORD"
+echo "PLAUSIBLE_SECRET_KEY: $PLAUSIBLE_SECRET_KEY"
